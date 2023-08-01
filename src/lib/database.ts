4684ae090ca56fdc05fb4471bc19b1ec7fa75c74
Jorge Castellon Jr <jorge@castellon.dev>
2023-07-31 23:24:47 -0700

    Maps images from database to use assets folder

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -2,17 +2,15 @@ import { CarouselItem } from "@/components/Carousel";
 import { faker } from "@faker-js/faker";
 
 export const getFakerShows = (count = 1) => {
-  return [...Array(count)].map((): CarouselItem => {
+  if (count < 8) count = 8;
+  return [...Array(count)].map((_, index): CarouselItem => {
     const title = faker.music.songName();
     const randomTagCount = Math.ceil(Math.random() * 3) + 1;
     return {
       id: title.replace(/\s/g, "-").toLowerCase(),
       title,
       description: faker.lorem.sentence(),
-      image: faker.image.urlLoremFlickr({
-        category: "anime",
-        width: 1000,
-      }),
+      image: `/assets/anime/anime-${index + 1}/logo.png`,
       headline: faker.lorem.words({ min: 1, max: 5 }) + "!",
       tags: [...Array(randomTagCount)].map(() =>
         faker.lorem.words({ min: 1, max: 2 })
@@ -29,11 +27,15 @@ export type AnimeEpisode = {
   season: number;
   episode: number;
   episode_title: string;
-  resume: number;
+  resume_percent: number;
+  min_left: number;
 };
 export const getFakerContinueWatching = (count = 1) => {
-  return [...Array(count)].map((): AnimeEpisode => {
+  return [...Array(count)].map((_, index): AnimeEpisode => {
     const title = faker.music.songName();
+    const show_percent = Math.ceil(Math.random() * 99);
+    const max_length = Math.ceil(Math.random() * 6) + 22;
+    const min_left = max_length - Math.round((show_percent / 100) * max_length);
     return {
       id: title.replace(/\s/g, "-").toLowerCase(),
       title,
@@ -46,7 +48,8 @@ export const getFakerContinueWatching = (count = 1) => {
       season: Math.ceil(Math.random() * 4),
       episode: Math.ceil(Math.random() * 24),
       episode_title: faker.lorem.words({ min: 1, max: 8 }),
-      resume: Math.ceil(Math.random() * 99),
+      min_left,
+      resume_percent: show_percent,
     };
   });
 };
